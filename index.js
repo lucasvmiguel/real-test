0a3f4658feac2ac419e9e5e8a171463df2a1ff4b
lucasvmiguel <lucasvieira.dev@gmail.com>
2016-01-23 14:18:59 -0200

    feat(issue #2)

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import read from 'read-file';
+import colors from 'colors/safe';
 
 import {boot} from './lib/boot';
 
-var config = JSON.parse(read.sync('config.json', 'utf8'));
+const FILE_CONFIG = process.env.npm_config_CONFIG || 'default';
 
-boot(config);
+try{
+  var config = JSON.parse(read.sync(`configs/${FILE_CONFIG}.json`, 'utf8'));
+  console.log(`>Config file: ${FILE_CONFIG}.json\n`);
+  boot(config);
+}catch(e){
+  console.log(colors.red('ERROR: it was not possible compile the tests, we will run the last compiled tests.\n\n'));
+}
